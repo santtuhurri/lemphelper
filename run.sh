350cc0834ac0c2a7aa4d8ba95a974d8b87bd630a
Santtu Hurri <hurri.santtu@gmail.com>
2022-05-16 19:19:38 +0300

    Add the script

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2018 Tero Karvinen http://TeroKarvinen.com GPL 3
+
+echo "Easy installer for LEMP  Starting... santtuhurri/lemphelper"
+
+echo "Installing Salt and Git..."
+sudo apt-get update
+sudo apt-get -y install git salt-minion
+
+echo "Retrieving and Applying Settings..."
+git clone https://github.com/santtuhurri/lemphelper.git
+cd lemphelper/
+
+sudo cp -R nginx /srv/salt
+sudo cp -R mariadb /srv/salt
+sudo cp -R php /srv/salt
+sudo cp top.sls /srv/salt/top.sls
+
+echo "You can now try the state with 'sudo salt-call --local state.apply'"
+echo "Done."
